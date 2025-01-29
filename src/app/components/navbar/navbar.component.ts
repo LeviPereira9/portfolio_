56147dcea28f7c1665e50924ba85f9e2993f4dd1
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-01-29 18:56:22 -0300

    navbar translate

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,14 @@
 import { Component, Host, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
+
+
+type navbar = {
+  home: string,
+  skills: string,
+  projects: string,
+  contactMe: string
+}
 
 @Component({
   selector: 'app-navbar',
@@ -16,10 +24,29 @@ export class NavbarComponent {
 
   isScrolled:boolean = false;
 
+  navbar:navbar = {
+    home: "",
+    skills: "",
+    projects: "",
+    contactMe: "",
+  }
+
   constructor(private translate: TranslateService){}
 
   @HostListener('window:scroll',[])
   onWindowScroll(){
     this.isScrolled = window.scrollY > 5;
   }
+
+   ngOnInit() {
+      this.getNavbar();
+
+      this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.getNavbar();
+    });
+    }
+
+    getNavbar(){
+      this.navbar = this.translate.instant('navbar');
+    }
 }
