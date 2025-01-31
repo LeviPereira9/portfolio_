bc3f6fd2d172f83be9968f38e2785e4e98ddf5e0
LeviPereira9 <102991893+LeviPereira9@users.noreply.github.com>
2025-01-31 07:44:13 -0300

    email

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -36,5 +36,23 @@ export class ContactComponent {
     this.contact = this.translate.instant('contactMe');
   }
 
+  sendEmail() {
+    const email = 'levi.pereira090@gmail.com';
+
+    const currentLang = this.translate.currentLang;
+
+    let subject: string;
+    let body: string;
+
+    if (currentLang === 'pt') {
+      subject = encodeURIComponent('Solicitação de Contato');
+      body = encodeURIComponent('Olá, gostaria de entrar em contato!');
+    } else {
+      subject = encodeURIComponent('Contact Request');
+      body = encodeURIComponent('Hello, I would like to get in touch!');
+    }
+
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  }
 
 }
